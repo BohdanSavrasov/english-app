d543a346d7b6f65633b90593793b87890d5b1e0c
Bohdan Savrasov <bohdan.savrasov@easternpeak.com>
2023-05-03 20:15:40 +0100

    scripts cleanup

--- data/all.py
+++
@@ -1,9 +0,0 @@
-from common import *
-from present_simple import present_simple
-
-if __name__ == "__main__":
-    show("We have not been smoking.")
-    # allTasksJson = json.dumps(present_simple())
-
-    # with open("./out.json", mode="w") as f:
-    #     f.write(allTasksJson)
